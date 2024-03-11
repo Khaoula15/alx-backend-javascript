11d10b67d9f5addae463cab16b80b1b5b27aff49
khaoula15 <khaoulasmp@gmail.com>
2024-03-11 17:05:34 +0000

    ES6 Basic

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+export default function getBudgetObject(income, gdp, capita) {
+  // returning  
+  // const budget = {income: income,gdp: gdp,capita: capita,}
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
